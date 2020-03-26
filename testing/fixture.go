7ebc178578a37138630a165fa3beb4fb6de633bf
Quan Tian <qtian@vmware.com>
2020-03-26 23:21:46 +0800

    Improve tracker performance

--- testing/fixture.go
+++ testing/fixture.go
@@ -189,7 +189,7 @@ type tracker struct {
 	scheme  ObjectScheme
 	decoder runtime.Decoder
 	lock    sync.RWMutex
-	objects map[schema.GroupVersionResource][]runtime.Object
+	objects map[schema.GroupVersionResource]map[types.NamespacedName]runtime.Object
 	// The value type of watchers is a map of which the key is either a namespace or
 	// all/non namespace aka "" and its value is list of fake watchers.
 	// Manipulations on resources will broadcast the notification events into the
@@ -206,7 +206,7 @@ func NewObjectTracker(scheme ObjectScheme, decoder runtime.Decoder) ObjectTracke
 	return &tracker{
 		scheme:   scheme,
 		decoder:  decoder,
-		objects:  make(map[schema.GroupVersionResource][]runtime.Object),
+		objects:  make(map[schema.GroupVersionResource]map[types.NamespacedName]runtime.Object),
 		watchers: make(map[schema.GroupVersionResource]map[string][]*watch.RaceFreeFakeWatcher),
 	}
 }
@@ -382,21 +382,23 @@ func (t *tracker) add(gvr schema.GroupVersionResource, obj runtime.Object, ns st
 		return errors.NewBadRequest(msg)
 	}
 
-	for i, existingObj := range t.objects[gvr] {
-		oldMeta, err := meta.Accessor(existingObj)
-		if err != nil {
-			return err
-		}
-		if oldMeta.GetNamespace() == newMeta.GetNamespace() && oldMeta.GetName() == newMeta.GetName() {
-			if replaceExisting {
-				for _, w := range t.getWatches(gvr, ns) {
-					w.Modify(obj)
-				}
-				t.objects[gvr][i] = obj
-				return nil
+	objs, ok := t.objects[gvr]
+	if !ok {
+		objs = map[types.NamespacedName]runtime.Object{}
+		t.objects[gvr] = objs
+	}
+
+	namespacedName := types.NamespacedName{Namespace: newMeta.GetNamespace(), Name: newMeta.GetName()}
+	_, ok = objs[namespacedName]
+	if ok {
+		if replaceExisting {
+			for _, w := range t.getWatches(gvr, ns) {
+				w.Modify(obj)
 			}
-			return errors.NewAlreadyExists(gr, newMeta.GetName())
+			t.objects[gvr][namespacedName] = obj
+			return nil
 		}
+		return errors.NewAlreadyExists(gr, newMeta.GetName())
 	}
 
 	if replaceExisting {
@@ -404,7 +406,7 @@ func (t *tracker) add(gvr schema.GroupVersionResource, obj runtime.Object, ns st
 		return errors.NewNotFound(gr, newMeta.GetName())
 	}
 
-	t.objects[gvr] = append(t.objects[gvr], obj)
+	t.objects[gvr][namespacedName] = obj
 
 	for _, w := range t.getWatches(gvr, ns) {
 		w.Add(obj)
@@ -434,36 +436,36 @@ func (t *tracker) Delete(gvr schema.GroupVersionResource, ns, name string) error
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	found := false
-
-	for i, existingObj := range t.objects[gvr] {
-		objMeta, err := meta.Accessor(existingObj)
-		if err != nil {
-			return err
-		}
-		if objMeta.GetNamespace() == ns && objMeta.GetName() == name {
-			obj := t.objects[gvr][i]
-			t.objects[gvr] = append(t.objects[gvr][:i], t.objects[gvr][i+1:]...)
-			for _, w := range t.getWatches(gvr, ns) {
-				w.Delete(obj)
-			}
-			found = true
-			break
-		}
+	objs, ok := t.objects[gvr]
+	if !ok {
+		return errors.NewNotFound(gvr.GroupResource(), name)
 	}
 
-	if found {
-		return nil
+	namespacedName := types.NamespacedName{Namespace: ns, Name: name}
+	obj, ok := objs[namespacedName]
+	if !ok {
+		return errors.NewNotFound(gvr.GroupResource(), name)
 	}
-
-	return errors.NewNotFound(gvr.GroupResource(), name)
+	delete(objs, namespacedName)
+	for _, w := range t.getWatches(gvr, ns) {
+		w.Delete(obj)
+	}
+	return nil
 }
 
 // filterByNamespaceAndName returns all objects in the collection that
 // match provided namespace and name. Empty namespace matches
 // non-namespaced objects.
-func filterByNamespaceAndName(objs []runtime.Object, ns, name string) ([]runtime.Object, error) {
+func filterByNamespaceAndName(objs map[types.NamespacedName]runtime.Object, ns, name string) ([]runtime.Object, error) {
 	var res []runtime.Object
+
+	if len(name) > 0 {
+		obj, ok := objs[types.NamespacedName{Namespace: ns, Name: name}]
+		if ok {
+			res = append(res, obj)
+		}
+		return res, nil
+	}
 
 	for _, obj := range objs {
 		acc, err := meta.Accessor(obj)
